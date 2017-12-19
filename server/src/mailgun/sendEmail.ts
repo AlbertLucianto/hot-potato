5e7f15ee811e883c8d50ba3b18a594d2f57700d9
Albert Lucianto <albertlucianto@gmail.com>
2017-12-20 03:53:42 +0800

    update schema and update lint

--- server/src/mailgun/sendEmail.ts
+++ server/src/mailgun/sendEmail.ts
@@ -1,65 +1,64 @@
-import * as fetch from 'isomorphic-fetch'
-import * as FormData from 'form-data'
-import { FunctionEvent } from 'graphcool-lib'
+import * as FormData from "form-data";
+import { FunctionEvent } from "graphcool-lib";
+import * as fetch from "isomorphic-fetch";
 
-
-interface EventData {
-  tag: string
-  from: string
-  to: [string]
-  subject: string
-  text: string
-  recipientVariables: Object | null
+interface IEventData {
+  tag: string;
+  from: string;
+  to: [string];
+  subject: string;
+  text: string;
+  recipientVariables: object | null;
 }
 
-export default async (event: FunctionEvent<EventData>) => {
-  console.log(event)
+export default async (event: FunctionEvent<IEventData>) => {
+  console.log(event);
 
-  if (!process.env['MAILGUN_API_KEY']) {
-    console.log('Please provide a valid mailgun secret key!')
-    return { error: 'Module not configured correctly.' }
+  if (!process.env.MAILGUN_API_KEY) {
+    console.log("Please provide a valid mailgun secret key!");
+    return { error: "Module not configured correctly." };
   }
 
-  if (!process.env['MAILGUN_DOMAIN']) {
-    console.log('Please provide a valid mailgun domain!')
-    return { error: 'Module not configured correctly.' }
+  if (!process.env.MAILGUN_DOMAIN) {
+    console.log("Please provide a valid mailgun domain!");
+    return { error: "Module not configured correctly." };
   }
 
   try {
-    const token = new Buffer(`api:${process.env['MAILGUN_API_KEY']}`).toString('base64')
-    const endpoint = `https://api.mailgun.net/v3/${process.env['MAILGUN_DOMAIN']}/messages`
+    const token = new Buffer(`api:${process.env.MAILGUN_API_KEY}`).toString("base64");
+    const endpoint = `https://api.mailgun.net/v3/${process.env.MAILGUN_DOMAIN}/messages`;
 
-    const { tag, to, from, subject, text } = event.data
-    const recipientVariables = event.data.recipientVariables || {}
+    const { tag, to, from, subject, text } = event.data;
+    const recipientVariables = event.data.recipientVariables || {};
 
     if (to.length > 1000) {
-      return { error: `Can't batch more than 1000 emails!` }
+      return { error: `Can't batch more than 1000 emails!` };
     }
 
-    const form = new FormData()
-    form.append('from', from)
+    const form = new FormData();
+    form.append("from", from);
 
-    for (var i = 0; i < to.length; i++) {
-      form.append('to', to[i])
+    for (const email of to) {
+      form.append("to", email);
     }
 
-    form.append('subject', subject)
-    form.append('text', text)
-    form.append('recipient-variables', JSON.stringify(recipientVariables))
+    form.append("subject", subject);
+    form.append("text", text);
+    form.append("recipient-variables", JSON.stringify(recipientVariables));
 
     const data = await fetch(endpoint, {
+      body: form,
       headers: {
-        'Authorization': `Basic ${token}`
+        Authorization: `Basic ${token}`,
       },
-      method: 'POST',
-      body: form
+      method: "POST",
     })
-      .then(response => response.json())
+      .then((response) => response.json());
 
-    return { data: { success: true } }
-  } catch(e) {
-    console.log(`Email could not be sent because an error occured:`)
-    console.log(e)
-    return { error: 'An unexpected error occured while sending email.' }
+    return { data: { success: true } };
+  } catch (e) {
+    console.log(`Email could not be sent because an error occured:`);
+    console.log(e);
+    return { error: "An unexpected error occured while sending email." };
   }
-}
+};
