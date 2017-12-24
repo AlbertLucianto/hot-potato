ba9d9bc8cd5718e524f2d14d9fa0c175b8ee17f5
Albert Lucianto <albertlucianto@gmail.com>
2017-12-25 03:15:02 +0800

    [client] - update and add interaction in New Potato page

---
+++ client/src/components/NewPotato/PotatoAnimate.vue
@@ -0,0 +1,107 @@
+<template>
+<svg class="potato" :width="width" viewBox="0 0 30 45" :style="potatoStyle">
+  <linearGradient id="linear-gradient" x1="14.94" y1="11.74" x2="14.94" y2="53.93" gradientUnits="userSpaceOnUse">
+    <stop offset="0" stop-color="#845c28"/>
+    <stop offset="1" stop-color="#ff3b30"/>
+  </linearGradient>
+  <path class="body" @mousedown="startDrag"
+    d="M13.16,42.24C-2,41.88-.21,29.07.39,20.9S7.67.23,15,0,28.19,4.89,28,13.86s3.34,11.69,1.31,21.34S17.93,42.36,13.16,42.24Z"/>
+  <g class="spots__group">
+    <circle class="spot" cx="5" cy="20" r="0.8"/>
+    <circle class="spot" cx="25.09" cy="26.59" r="1.01"/>
+    <circle class="spot" cx="14.94" cy="23.35" r="0.79"/>
+    <circle class="spot" cx="20.98" cy="17.99" r="1.49"/>
+    <circle class="spot" cx="20.18" cy="8.93" r="0.79"/>
+  </g>
+</svg>
+</template>
+
+<script>
+import dynamics from 'dynamics.js';
+
+const START_POTATO_WIDTH = 150;
+const DAMP_POSITIVE_Y_DRAG = 4;
+const DAMP_NEGATIVE_Y_DRAG = 1.5;
+const DAMP_X_DRAG = 5;
+
+export default {
+  data() {
+    return {
+      width: START_POTATO_WIDTH,
+      start: { x: 0, y: 0 },
+      pos: { x: 0, y: 0 },
+      dragging: false,
+    };
+  },
+  computed: {
+    potatoStyle() {
+      const x = this.pos.x / DAMP_X_DRAG;
+      const posY = this.pos.y;
+      const y = posY > 0 ? posY / DAMP_POSITIVE_Y_DRAG : posY / DAMP_NEGATIVE_Y_DRAG;
+      return {
+        transform: `translate(${x}px, ${y}px)`,
+      };
+    },
+  },
+  methods: {
+    startDrag(e) {
+      const evt = e.changedTouches ? e.changedTouches[0] : e;
+      dynamics.stop(this.pos);
+      this.dragging = true;
+      this.start.x = evt.pageX - this.pos.x;
+      this.start.y = evt.pageY - this.pos.y;
+      window.addEventListener('mousemove', this.onDrag);
+      window.addEventListener('mouseup', this.onRelease);
+      window.addEventListener('touchmove', this.onDrag);
+      window.addEventListener('touchend', this.onRelease);
+    },
+    onDrag(e) {
+      if (this.dragging) {
+        const evt = e.changedTouches ? e.changedTouches[0] : e;
+        this.pos.x = evt.pageX - this.start.x;
+        this.pos.y = evt.pageY - this.start.y;
+      }
+    },
+    onRelease() {
+      if (this.dragging) {
+        this.dragging = false;
+        dynamics.animate(this.pos, {
+          x: 0,
+          y: 0,
+        }, {
+          type: dynamics.spring,
+          duration: 1000,
+          frequency: 300,
+          friction: 300,
+        });
+        window.removeEventListener('mousemove', this.onDrag);
+        window.removeEventListener('mouseup', this.onRelease);
+        window.removeEventListener('touchmove', this.onDrag);
+        window.removeEventListener('touchend', this.onRelease);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.potato {
+  transition: width 1s ease;
+  .body {
+    fill: url(#linear-gradient);
+    cursor: grab;
+  }
+  .spot{
+    fill: #e5901a;
+  }
+  .spots__group {
+    animation: spots 5s ease alternate infinite;
+  }
+}
+
+@keyframes spots {
+  100% {
+    transform: translateX(2.5px);
+  }
+}
+</style>
--- client/src/components/NewPotato/index.vue
+++ client/src/components/NewPotato/index.vue
@@ -1,11 +1,30 @@
 <template>
-<div>New Potato</div>  
+<div class="newPotatoPage__container">
+  <potato-animate/>
+</div>
 </template>
 
 <script>
-export default {};
+import PotatoAnimate from './PotatoAnimate';
+
+export default {
+  components: {
+    PotatoAnimate,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+div {
+  box-sizing: border-box;
+}
+.newPotatoPage__container {
+  height: 100%;
+  width: 100%;
+  padding: 50px;
+  background: #232222;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
