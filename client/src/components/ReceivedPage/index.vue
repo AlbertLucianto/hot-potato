372ea1d6e2ace6501c845c5a28a42e17a9998506
Albert Lucianto <albertlucianto@gmail.com>
2018-01-01 23:27:51 +0800

    [client] - add basic query received page

--- client/src/components/ReceivedPage/index.vue
+++ client/src/components/ReceivedPage/index.vue
@@ -1,9 +1,49 @@
 <template>
-<div>Received</div>  
+<div>{{ receivedPotato }}</div>  
 </template>
 
 <script>
-export default {};
+import gql from 'graphql-tag';
+
+export default {
+  props: {
+    userId: String,
+  },
+  data() {
+    return {
+      loading: 0,
+    };
+  },
+  apollo: {
+    $loadingKey: 'loading',
+    receivedPotato() {
+      return {
+        query: gql`query received($currentlyHold: Boolean!, $filterDropped: Boolean!) {
+          receivedPotato(
+            currentlyHold: $currentlyHold,
+            filterDropped: $filterDropped,
+          ) {
+            id
+            droppedDate
+          }
+        }`,
+        variables() {
+          return {
+            currentlyHold: false,
+            filterDropped: true,
+          };
+        },
+        skip() {
+          return !this.userId;
+        },
+        fetchPolicy: 'cache-and-network',
+        result(data) {
+          console.log(data); // eslint-disable-line no-console
+        },
+      };
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
