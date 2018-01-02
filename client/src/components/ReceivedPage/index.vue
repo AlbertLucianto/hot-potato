bb24fb24a09e4c1736cf246891b451f0e77499ec
Albert Lucianto <albertlucianto@gmail.com>
2018-01-02 22:58:59 +0800

    [client] - update receivedPage

--- client/src/components/ReceivedPage/index.vue
+++ client/src/components/ReceivedPage/index.vue
@@ -1,5 +1,24 @@
 <template>
-<div>{{ receivedPotato }}</div>  
+<div class="receivedList__container">
+  <div v-for="(potato, idx) in receivedPotato" :key="potato.id" class="received__item">
+    <div v-if="categoryByDate(potato.droppedDate) === category.dropped" class="received__content">
+      <img class="potatoIcon" src="../../assets/SVG/list_dropped.svg"/>
+      <div class="content__text">{{ displayText(potato.droppedDate).value }}</div>
+    </div>
+    <div v-else-if="categoryByDate(potato.droppedDate) === category.relax" class="received__content">
+      <img class="potatoIcon" src="../../assets/SVG/list_relax.svg"/>
+      <div class="content__text">{{ displayText(potato.droppedDate).value }}</div>
+    </div>
+    <div v-else-if="categoryByDate(potato.droppedDate) === category.medium" class="received__content">
+      <img class="potatoIcon" src="../../assets/SVG/list_medium.svg"/>
+      <div class="content__text">{{ displayText(potato.droppedDate).value }}</div>
+    </div>
+    <div v-else-if="categoryByDate(potato.droppedDate) === category.urgent" class="received__content">
+      <img class="potatoIcon" src="../../assets/SVG/list_urgent.svg"/>
+      <div class="content__text">{{ displayText(potato.droppedDate).value }}</div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -12,7 +31,43 @@ export default {
   data() {
     return {
       loading: 0,
+      category: {
+        dropped: 'DROPPED',
+        relax: 'RELAX',
+        medium: 'MEDIUM',
+        urgent: 'URGENT',
+      },
     };
+  },
+  computed: {
+    categoryByDate() {
+      return (ISODate) => {
+        if (new Date(ISODate) < new Date()) return this.category.dropped;
+        else return this.category.medium;
+      };
+    },
+    displayText() {
+      return (ISODate) => {
+        if (new Date(ISODate) < new Date()) return {
+          value: `Dropped on ${new Date(ISODate).toDateString()}`,
+        };
+        const dayDiff = Math.floor((new Date(ISODate).getTime() - new Date().getTime()) / (1000 * 3600 * 24));
+        if (dayDiff >= 1) return {
+          value: dayDiff,
+          scale: `day${dayDiff > 1 ? 's' : ''}`,
+        };
+        const hourDiff = new Date(ISODate).getHours() - new Date().getHours();
+        if (hourDiff >= 1) return {
+          value: hourDiff,
+          scale: `hour${hourDiff > 1 ? 's' : ''}`,
+        };
+        const minDiff = new Date(ISODate).getMinutes() - new Date().getMinutes();
+        if (minDiff >= 0) return {
+          value: minDiff,
+          scale: `minute${minDiff > 1 ? 's' : ''}`,
+        };
+      };
+    }
   },
   apollo: {
     $loadingKey: 'loading',
@@ -30,7 +85,7 @@ export default {
         variables() {
           return {
             currentlyHold: false,
-            filterDropped: true,
+            filterDropped: false,
           };
         },
         skip() {
@@ -47,5 +102,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.receivedList__container {
+  display: flex;
+  justify-content: space-around;
+  .received__item {
+    width: 120px;
+    height: 120px;
+    margin: 30px;
+    .potatoIcon {
+      max-height: 100px;
+      max-width: 100px;
+    }
+  }
+}
 </style>
