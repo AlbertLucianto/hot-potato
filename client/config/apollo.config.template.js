33c543efb6bd666b0abc4bda112735f0a755292f
Albert Lucianto <albertlucianto@gmail.com>
2018-01-10 16:12:23 +0800

    update apollo config templage

--- client/config/apollo.config.template.js
+++ client/config/apollo.config.template.js
@@ -3,6 +3,8 @@
  * then change its name into `apollo.config.js`.
  */
 
-module.export = {
+const configs = {
   uri: '<your_graphql_api_endpoint>', // Mine is: 'http://localhost:60000/simple/v1/cjbeszs0u00020127v5x15uuc'
 };
+
+export default configs;
