89557c2aa00a98cd891eb7e18bc52a5cf5f553bc
Albert Lucianto <albertlucianto@gmail.com>
2018-01-14 23:48:10 +0800

    [client] - add basic send button in receivedPage

--- client/src/components/ReceivedPage/index.vue
+++ client/src/components/ReceivedPage/index.vue
@@ -6,6 +6,7 @@
   <div class="gradient--botom" />
   <search-bar :setSearch="setSearch" :selectedUser="selectedUser" :selectUser="selectUser" v-if="selected"
     :jerked="!!notification" :cancel="cancelSearch" :setSearching="setSearching" :results="allUsers"/>
+  <div class="sendButton" v-if="selectedUser && selected">SEND</div>
 </div>
 </template>
 
@@ -97,11 +98,22 @@ export default {
     setSearching(val) { this.searching = val; },
     cancelSearch() { this.selectedUser = undefined; },
     selectUser(user) { this.selectedUser = user; },
+    passPotato() {
+      this.$apollo.mutate({
+        mutation: gql``,
+      });
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+$pink: rgb(255,45,85);
+$red: rgb(255,59,48);
+$purple: rgb(88,86,214);
+$orange: rgb(255,149,0);
+$darkOrange: rgb(245,140,0);
+
 .receivedList__container {
   box-sizing: border-box;
   display: flex;
@@ -119,6 +131,19 @@ export default {
     height: 120px;
     background: #F8F8FE;
     box-shadow: 0 0 35px -5px rgba(150,78,78,.2);
+  }
+  .sendButton {
+    position: absolute;
+    bottom: 200px;
+    padding: 10px 20px;
+    border-radius: 10px;
+    box-shadow: 0 5px 30px -5px rgba(0,0,0,.3);
+    background: white;
+    transition: all .1s ease;
+    &:hover {
+      box-shadow: 0 2.5px 5px rgba(0,0,0,.3);
+      cursor: pointer;
+    }
   }
 }
 </style>
