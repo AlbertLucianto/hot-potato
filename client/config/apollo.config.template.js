6c631762ce484e0f584a433022752271fa74f8ab
Albert Lucianto <albertlucianto@gmail.com>
2018-01-21 23:21:08 +0800

    [client] - update apollo configs template for production

--- client/config/apollo.config.template.js
+++ client/config/apollo.config.template.js
@@ -3,8 +3,15 @@
  * then change its name into `apollo.config.js`.
  */
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const configs = {
-  uri: '<your_graphql_api_endpoint>', // Mine is: 'http://localhost:60000/simple/v1/cjbeszs0u00020127v5x15uuc'
+  dev: {
+    uri: '<your_dev_graphql_api_endpoint>', // Mine is: 'http://localhost:60000/simple/v1/cjbeszs0u00020127v5x15uuc'
+  },
+  prod: {
+    uri: '<your_production_graphql_api_endpoint>',
+  },
 };
 
-export default configs;
+export default isProduction ? configs.prod : configs.dev;
